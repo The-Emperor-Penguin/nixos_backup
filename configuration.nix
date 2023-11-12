442ae54f1beec27a98271e5e5820a2ccbb938883
Josiah Snyder <josiahsnyder@yahoo.com>
2023-11-12 13:28:18 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,15 +4,14 @@
 
 {config, pkgs, ... }:
 let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  home-manager = fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
 in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
+      (import "${home-manager}/nixos")
     ];
 
   nixpkgs.config = {
@@ -26,15 +25,24 @@ in
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
 
+  zramSwap.enable = true;
+
+  systemd.oomd = {
+    enable = true;
+    enableRootSlice = true;
+    enableUserServices = true;
+  };
+
   # Bootloader.
   boot.loader.timeout = 3;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernel.sysctl = {
   "vm.max_map_count" = 262144;
   };
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -73,8 +81,8 @@ in
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -114,22 +122,16 @@ in
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.josiah = { lib, ... }: {
     home.username = "josiah";
     home.homeDirectory = "/home/josiah";
     home.stateVersion = "22.11";
-    home.packages = [ pkgs.vscode
-                      pkgs.keepassxc
+    home.packages = [ pkgs.keepassxc
                       pkgs.virt-manager
                       pkgs.unstable.godot_4
+                      pkgs.unstable.r2modman
                       pkgs.unstable.blender-hip ];
 
     programs.direnv.enable = true;
@@ -137,419 +139,20 @@ in
     
     programs.bash.enable = true;
 
-    services.easyeffects = {
+    programs.vscode = {
       enable = true;
-      preset = "default";
+      mutableExtensionsDir = false;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        redhat.java
+        vscjava.vscode-java-dependency
+        vscjava.vscode-java-debug
+        vscjava.vscode-java-test
+        arrterian.nix-env-selector
+        mkhl.direnv
+        ms-python.python
+      ];
     };
-
-    dconf.settings = {
-      "org/gnome/desktop/interface" = { 
-        gtk-theme = "Adwaita-dark"; 
-      };
-    };
-
-    home.file.".config/mako/config".text = ''
-      default-timeout=1000
-      max-visible=3
-      layer=top
-    '';
-
-    home.file.".config/wofi/config".text = ''
-      location = 2
-      show = drun
-    '';
-
-    home.file.".config/waybar/style.css".text = ''
-      * {
-        font-size: 16px;
-        font-family: monospace;
-      }
-
-      window#waybar {
-        background: transparent;
-        color: #1dbd65;
-      }
-
-      #custom-right-arrow-dark,
-      #custom-left-arrow-dark {
-        color: #1a1a1a;
-      }
-      #custom-right-arrow-light,
-      #custom-left-arrow-light {
-        color: #292b2e;
-      }
-      
-      #network,
-      #idle_inhibitor,
-      #workspaces,
-      #clock.1,
-      #clock.2,
-      #clock.3,
-      #clock,
-      #pulseaudio,
-      #memory,
-      #cpu,
-      #battery,
-      #disk,
-      #tray {
-        color: #1dbd65;
-        border-radius: 25px;
-        background: #064169;
-        padding: 0 10px;
-      }
-
-      #workspaces button {
-        padding: 0 2px;
-        color: #fdf6e3;
-      }
-      #workspaces button.focused {
-        color: #268bd2;
-      }
-      #workspaces button:hover {
-        box-shadow: inherit;
-        text-shadow: inherit;
-      }
-      #workspaces button:hover {
-        background: #1a1a1a;
-        border: #1a1a1a;
-        padding: 0 3px;
-      }
-    '';
-
-    home.file.".config/waybar/config".text = ''
-      {
-          "layer": "top", // Waybar at top layer
-          // "position": "bottom", // Waybar position (top|bottom|left|right)
-          "height": 25, // Waybar height (to be removed for auto height)
-          // "width": 1280, // Waybar width
-          "spacing": 5, // Gaps between modules (4px)
-          // Choose the order of the modules
-          "modules-left": ["pulseaudio", "network"],
-          "modules-center": ["clock"],
-          "modules-right": ["idle_inhibitor","tray", "cpu", "memory"],
-          // Modules configuration
-          // "sway/workspaces": {
-          //     "disable-scroll": true,
-          //     "all-outputs": true,
-          //     "format": "{name}: {icon}",
-          //     "format-icons": {
-          //         "1": "",
-          //         "2": "",
-          //         "3": "",
-          //         "4": "",
-          //         "5": "",
-          //         "urgent": "",
-          //         "focused": "",
-          //         "default": ""
-          //     }
-          // },
-          "keyboard-state": {
-              "numlock": true,
-              "capslock": true,
-              "format": "{name} {icon}",
-              "format-icons": {
-                  "locked": "",
-                  "unlocked": ""
-              }
-          },
-          "sway/mode": {
-              "format": "<span style=\"italic\">{}</span>"
-          },
-          "sway/scratchpad": {
-              "format": "{icon} {count}",
-              "show-empty": false,
-              "format-icons": ["", ""],
-              "tooltip": true,
-              "tooltip-format": "{app}: {title}"
-          },
-          "mpd": {
-              "format": "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ",
-              "format-disconnected": "Disconnected ",
-              "format-stopped": "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ",
-              "unknown-tag": "N/A",
-              "interval": 2,
-              "consume-icons": {
-                  "on": " "
-              },
-              "random-icons": {
-                  "off": "<span color=\"#f53c3c\"></span> ",
-                  "on": " "
-              },
-              "repeat-icons": {
-                  "on": " "
-              },
-              "single-icons": {
-                  "on": "1 "
-              },
-              "state-icons": {
-                  "paused": "",
-                  "playing": ""
-              },
-              "tooltip-format": "MPD (connected)",
-              "tooltip-format-disconnected": "MPD (disconnected)"
-          },
-          "idle_inhibitor": {
-              "format": "{icon}",
-              "format-icons": {
-                  "activated": "",
-                  "deactivated": ""
-              }
-          },
-          "tray": {
-              //"icon-size": 20,
-              "spacing": 10
-          },
-          "clock": {
-              "timezone": "America/Chicago",
-              "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>",
-              "format-alt": "{:%Y-%m-%d}"
-          },
-          "cpu": {
-              "format": "{usage}% ",
-              "tooltip": false
-          },
-          "memory": {
-              "format": "{}% "
-          },
-          "temperature": {
-              // "thermal-zone": 2,
-              // "hwmon-path": "/sys/class/hwmon/hwmon2/temp1_input",
-              "critical-threshold": 80,
-              // "format-critical": "{temperatureC}°C {icon}",
-              "format": "{temperatureC}°C {icon}",
-              "format-icons": ["", "", ""]
-          },
-          "backlight": {
-              // "device": "acpi_video1",
-              "format": "{percent}% {icon}",
-              "format-icons": ["", "", "", "", "", "", "", "", ""]
-          },
-          "battery": {
-              "states": {
-                  // "good": 95,
-                  "warning": 30,
-                  "critical": 15
-              },
-              "format": "{capacity}% {icon}",
-              "format-charging": "{capacity}% ",
-              "format-plugged": "{capacity}% ",
-              "format-alt": "{time} {icon}",
-              // "format-good": "", // An empty format will hide the module
-              // "format-full": "",
-              "format-icons": ["", "", "", "", ""]
-          },
-          "battery#bat2": {
-              "bat": "BAT2"
-          },
-          "network": {
-              // "interface": "wlp2*", // (Optional) To force the use of this interface
-              "format-wifi": "{essid} ({signalStrength}%) ",
-              "format-ethernet": "{ipaddr}/{cidr} ",
-              "tooltip-format": "{ifname} via {gwaddr} ",
-              "format-linked": "{ifname} (No IP) ",
-              "format-disconnected": "Disconnected ⚠",
-              "format-alt": "{ifname}: {ipaddr}/{cidr}"
-          },
-          "pulseaudio": {
-              // "scroll-step": 1, // %, can be a float
-              "format": "{volume}% {icon} {format_source}",
-              "format-bluetooth": "{volume}% {icon} {format_source}",
-              "format-bluetooth-muted": " {icon} {format_source}",
-              "format-muted": " {format_source}",
-              "format-source": "{volume}% ",
-              "format-source-muted": "",
-              "format-icons": {
-                  "headphone": "",
-                  "hands-free": "",
-                  "headset": "",
-                  "phone": "",
-                  "portable": "",
-                  "car": "",
-                  "default": ["", "", ""]
-              },
-              "on-click": "pavucontrol"
-          },
-          "custom/media": {
-              "format": "{icon} {}",
-              "return-type": "json",
-              "max-length": 40,
-              "format-icons": {
-                  "spotify": "",
-                  "default": "🎜"
-              },
-              "escape": true,
-              "exec": "$HOME/.config/waybar/mediaplayer.py 2> /dev/null" // Script in resources folder
-              // "exec": "$HOME/.config/waybar/mediaplayer.py --player spotify 2> /dev/null" // Filter player based on name
-          }
-      }
-    '';
-    
-    home.file.".config/hypr/hyprland.conf".text = ''
-      exec-once=waybar & ${pkgs.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1 & mako & swaybg --image /home/josiah/.config/background.jpg
-      # This is an example Hyprland config file.
-      #
-      # Refer to the wiki for more information.
-
-      #
-      # Please note not all available settings / options are set here.
-      # For a full list, see the wiki
-      #
-
-      # See https://wiki.hyprland.org/Configuring/Monitors/
-      monitor=,preferred,auto,auto
-
-
-      # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-
-      # Execute your favorite apps at launch
-      # exec-once = waybar & hyprpaper & firefox
-
-      # Source a file (multi-file configs)
-      # source = ~/.config/hypr/myColors.conf
-
-      # Some default env vars.
-      env = XCURSOR_SIZE,24
-
-      # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
-      input {
-          kb_layout = us
-          kb_variant =
-          kb_model =
-          kb_options =
-          kb_rules =
-
-          follow_mouse = 1
-
-          touchpad {
-              natural_scroll = false
-          }
-
-          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-      }
-
-      general {
-          # See https://wiki.hyprland.org/Configuring/Variables/ for more
-
-          gaps_in = 5
-          gaps_out = 20
-          border_size = 2
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-          col.inactive_border = rgba(595959aa)
-
-          layout = dwindle
-      }
-
-      decoration {
-          # See https://wiki.hyprland.org/Configuring/Variables/ for more
-
-          rounding = 10
-          blur = true
-          blur_size = 3
-          blur_passes = 1
-          blur_new_optimizations = true
-
-          drop_shadow = true
-          shadow_range = 4
-          shadow_render_power = 3
-          col.shadow = rgba(1a1a1aee)
-      }
-
-      animations {
-          enabled = true
-
-          # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-
-          bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
-          animation = windows, 1, 7, myBezier
-          animation = windowsOut, 1, 7, default, popin 80%
-          animation = border, 1, 10, default
-          animation = borderangle, 1, 8, default
-          animation = fade, 1, 7, default
-          animation = workspaces, 1, 6, default
-      }
-
-      dwindle {
-          # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-          pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-          preserve_split = true # you probably want this
-      }
-
-      master {
-          # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-          new_is_master = true
-      }
-
-      gestures {
-          # See https://wiki.hyprland.org/Configuring/Variables/ for more
-          workspace_swipe = false
-      }
-
-      # Example per-device config
-      # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
-      device:epic-mouse-v1 {
-          sensitivity = -0.5
-      }
-
-      # Example windowrule v1
-      # windowrule = float, ^(kitty)$
-      # Example windowrule v2
-      # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
-      # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-
-
-      # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-      $mainMod = SUPER
-
-      # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      bind = $mainMod, Q, exec, kitty
-      bind = $mainMod, C, killactive,
-      bind = $mainMod, M, exit,
-      bind = $mainMod, E, exec, dolphin
-      bind = $mainMod, V, togglefloating,
-      bind = $mainMod, R, exec, wofi --show drun
-      bind = $mainMod, P, pseudo, # dwindle
-      bind = $mainMod, J, togglesplit, # dwindle
-      bind = $mainMod, print, exec, grim
-
-      # Move focus with mainMod + arrow keys
-      bind = $mainMod, left, movefocus, l
-      bind = $mainMod, right, movefocus, r
-      bind = $mainMod, up, movefocus, u
-      bind = $mainMod, down, movefocus, d
-
-      # Switch workspaces with mainMod + [0-9]
-      bind = $mainMod, 1, workspace, 1
-      bind = $mainMod, 2, workspace, 2
-      bind = $mainMod, 3, workspace, 3
-      bind = $mainMod, 4, workspace, 4
-      bind = $mainMod, 5, workspace, 5
-      bind = $mainMod, 6, workspace, 6
-      bind = $mainMod, 7, workspace, 7
-      bind = $mainMod, 8, workspace, 8
-      bind = $mainMod, 9, workspace, 9
-      bind = $mainMod, 0, workspace, 10
-
-      # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      bind = $mainMod SHIFT, 1, movetoworkspace, 1
-      bind = $mainMod SHIFT, 2, movetoworkspace, 2
-      bind = $mainMod SHIFT, 3, movetoworkspace, 3
-      bind = $mainMod SHIFT, 4, movetoworkspace, 4
-      bind = $mainMod SHIFT, 5, movetoworkspace, 5
-      bind = $mainMod SHIFT, 6, movetoworkspace, 6
-      bind = $mainMod SHIFT, 7, movetoworkspace, 7
-      bind = $mainMod SHIFT, 8, movetoworkspace, 8
-      bind = $mainMod SHIFT, 9, movetoworkspace, 9
-      bind = $mainMod SHIFT, 0, movetoworkspace, 10
-
-      # Scroll through existing workspaces with mainMod + scroll
-      bind = $mainMod, mouse_down, workspace, e+1
-      bind = $mainMod, mouse_up, workspace, e-1
-
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = $mainMod, mouse:272, movewindow
-      bindm = $mainMod, mouse:273, resizewindow
-    '';
 
     programs.firefox = {
       enable = true;
@@ -590,13 +193,17 @@ in
               installation_mode = "force_installed";
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/startpage-private-search/latest.xpi";
             };
-            "graze_for_mastodon@jaredzimmerman.com" = {
-              installation_mode = "force_installed";
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/graze/latest.xpi";
-            };
             "streetpass@streetpass.social" = {
               installation_mode = "force_installed";
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/strretpass-for-mastodon/latest.xpi";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/streetpass-for-mastodon/latest.xpi";
+            };
+            "mastodon-auto-remote-follow@rugk.github.io" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/mastodon-simplified-federation/latest.xpi";
+            };
+            "@testpilot-containers" = {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
             };
           };
         };
@@ -614,30 +221,15 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    swaybg
-    vlc
     easyeffects
-    rustc
-    cargo
-    python311
-    git
-    gh
-    kitty
-    wayland
-    glib
-    wofi
-    waybar
-    wdisplays
-    mako
-    grim
-    pavucontrol
-    libsForQt5.polkit-kde-agent
-    xfce.thunar
-    xfce.thunar-volman
-    xfce.thunar-archive-plugin
-    xfce.thunar-media-tags-plugin
+    vlc
+    libreoffice-qt
+    hunspell
+    hunspellDicts.en_US
+    gimp
   ];
   
+  programs.kdeconnect.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -673,9 +265,11 @@ in
   };
   
   services.flatpak.enable = true;
-  
+  services.packagekit.enable = true;  
+
   system.autoUpgrade = {
   enable = true;
   };
 
 }
+
